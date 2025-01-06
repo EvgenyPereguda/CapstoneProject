e476e18b7d6217fbaa1f9abbe18fd93b2a58b458
E P <e34476545@gmail.com>
2025-01-06 22:32:42 +1100

    Add multipage support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    createHtmlPlugin({
+      minify: false,
+      pages: [
+        {
+          filename: 'index.html',
+          template: 'index.html'
+        },
+        {
+          filename: 'managment',
+          template: 'managment.html'
+        },
+      ],
+    }),
+  ],
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
